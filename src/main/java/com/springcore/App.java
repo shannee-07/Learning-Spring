059256501a33f668829d975fd96a313691c1a45f
Shannee Ahirwar <shanneeahirwar@gmail.com>
2024-12-07 14:57:36 +0530

    Added more concise ways to defind bean

--- src/main/java/com/springcore/App.java
+++ src/main/java/com/springcore/App.java
@@ -10,6 +10,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		@SuppressWarnings("resource")
 		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
 		Student student1 = (Student) context.getBean("student1");
 		System.out.println(student1.toString());
